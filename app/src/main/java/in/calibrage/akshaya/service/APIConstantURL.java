ee9f31af05ed335471fb5784ee46c5cc7bcdd870
Calibrage21 <roja@calibrage.in>
2020-06-03 15:38:09 -0700

    test

--- app/src/main/java/in/calibrage/akshaya/service/APIConstantURL.java
+++ app/src/main/java/in/calibrage/akshaya/service/APIConstantURL.java
@@ -1,9 +1,9 @@
 package in.calibrage.akshaya.service;
 
 public interface APIConstantURL {
- //public static  final  String LOCAL_URL="http://103.241.144.240:9096/api/";//test
+ public static  final  String LOCAL_URL="http://103.241.144.240:9096/api/";//test
 //public static  final  String LOCAL_URL="http://183.82.111.111/3FFarmerAPI/api/";
-public static  final  String LOCAL_URL="http://103.241.144.240:9098/api/";//live
+//public static  final  String LOCAL_URL="http://103.241.144.240:9098/api/";//live
     //String LookUpCategory = "GetActiveLookUp/9";
     String LookUpCategory = "GetActiveLookUp/9";
     String banner ="Banner";
